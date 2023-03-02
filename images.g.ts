e8e27507b8b79098835b7bb76f8974c8e55b3896
ollieg430 <72669911+ollieg430@users.noreply.github.com>
2023-03-02 08:45:01 -0500

    Update pxt.json, main.blocks, main.ts, images.g.jres, images.g.ts

---
+++ images.g.ts
@@ -0,0 +1,55 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "WizzardHatIcon":return img`
+. . . . . . . . 
+. . . . . . . . 
+. . . 8 8 . . . 
+. . 8 8 8 8 . . 
+. 8 8 8 8 8 8 . 
+8 8 8 8 8 8 8 8 
+. . . . . . . . 
+. . . . . . . . 
+`;
+            case "image2":
+            case "Cursor":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . f f f f f 
+. . . . . . . . . . . f 1 1 1 f 
+. . . . . . . . . . . f 1 1 1 f 
+. . . . . . . . . . . f 1 1 1 f 
+f f f f f f f f f f f f 1 1 1 f 
+f 1 1 f 1 1 f 1 f 1 1 f 1 1 1 f 
+f 1 1 f 1 1 f 1 f 1 1 f 1 1 1 f 
+f 1 1 f 1 1 f 1 f 1 1 f 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 f 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f f 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+. f 1 1 1 1 1 1 1 1 1 1 1 f f f 
+. f f f f f f f f f f f f f f . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
--- main.ts
+++ main.ts
@@ -1,3 +1,16 @@
+namespace SpriteKind {
+    export const Icon = SpriteKind.create()
+    export const Tower = SpriteKind.create()
+}
+function MakeATower (Name: Image, Image2: Image, Cost: number) {
+    NewTower = sprites.create(Image2, SpriteKind.Tower)
+    sprites.setDataString(NewTower, "name", "")
+}
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (Cursor.overlapsWith(WizzardHat)) {
+    	
+    }
+})
 scene.onHitWall(SpriteKind.Enemy, function (sprite, location) {
     if (tiles.tileIs(tiles.locationOfSprite(sprite), assets.tile`myTile6`)) {
         sprite.vy = 0
@@ -8,11 +21,20 @@ scene.onHitWall(SpriteKind.Enemy, function (sprite, location) {
     }
 })
 let NewEnemy: Sprite = null
+let NewTower: Sprite = null
+let Cursor: Sprite = null
+let WizzardHat: Sprite = null
 let EnemySpeed = 0
 tiles.loadMap(tiles.createSmallMap(tilemap`level2`))
 EnemySpeed = 50
 tiles.coverAllTiles(assets.tile`myTile6`, assets.tile`myTile1`)
 tiles.coverAllTiles(assets.tile`myTile7`, assets.tile`myTile1`)
+WizzardHat = sprites.create(assets.image`WizzardHatIcon`, SpriteKind.Icon)
+Cursor = sprites.create(assets.image`Cursor`, SpriteKind.Player)
+controller.moveSprite(Cursor, 70, 70)
+Cursor.setFlag(SpriteFlag.GhostThroughWalls, true)
+WizzardHat.x = 105
+WizzardHat.y = 70
 game.onUpdateInterval(500, function () {
     NewEnemy = sprites.create(img`
         3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
