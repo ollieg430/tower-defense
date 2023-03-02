b5227a816f53ac574a6a1bd3c9d00bd4d400ee28
ollieg430 <72669911+ollieg430@users.noreply.github.com>
2023-03-02 08:56:49 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,13 +2,33 @@ namespace SpriteKind {
     export const Icon = SpriteKind.create()
     export const Tower = SpriteKind.create()
 }
-function MakeATower (Name: Image, Image2: Image, Cost: number) {
+function MakeATower (Image2: Image, Cost: number, name: string) {
     NewTower = sprites.create(Image2, SpriteKind.Tower)
-    sprites.setDataString(NewTower, "name", "")
+    sprites.setDataNumber(NewTower, "cost", Cost)
+    sprites.setDataString(NewTower, "name", name)
+    return NewTower
 }
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     if (Cursor.overlapsWith(WizzardHat)) {
-    	
+        thingWeAreHolding = MakeATower(img`
+            . . . . . . c c c . . . . . . . 
+            . . . . . . c 8 b c . . . . . . 
+            . . . . c c c 8 8 c c c . . . . 
+            . . c c c c 8 8 8 8 c b c c . . 
+            . c b b 8 b 8 8 8 8 b 8 b b c . 
+            . c b 8 8 b b 8 8 b b 8 8 b c . 
+            . . c 8 8 8 b b b b 8 8 8 f . . 
+            . . f f 8 8 8 8 8 8 8 8 f f . . 
+            . . f f f b f e e f b f f f . . 
+            . . f f f 1 f b b f 1 f f f . . 
+            . . . f f b b b b b b f f . . . 
+            . . . e e f e e e e f e e . . . 
+            . . e b f b 8 b b 8 b c b e . . 
+            . . e e f 8 8 8 8 8 8 f e e . . 
+            . . . . c b 8 8 8 8 b c . . . . 
+            . . . . . f f f f f f . . . . . 
+            `, 1, "Wizzard")
+        Cursor.setFlag(SpriteFlag.Invisible, true)
     }
 })
 scene.onHitWall(SpriteKind.Enemy, function (sprite, location) {
@@ -21,6 +41,7 @@ scene.onHitWall(SpriteKind.Enemy, function (sprite, location) {
     }
 })
 let NewEnemy: Sprite = null
+let thingWeAreHolding: Sprite = null
 let NewTower: Sprite = null
 let Cursor: Sprite = null
 let WizzardHat: Sprite = null
@@ -35,6 +56,11 @@ controller.moveSprite(Cursor, 70, 70)
 Cursor.setFlag(SpriteFlag.GhostThroughWalls, true)
 WizzardHat.x = 105
 WizzardHat.y = 70
+game.onUpdate(function () {
+    if (thingWeAreHolding) {
+        thingWeAreHolding.setPosition(Cursor.x, Cursor.y)
+    }
+})
 game.onUpdateInterval(500, function () {
     NewEnemy = sprites.create(img`
         3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
